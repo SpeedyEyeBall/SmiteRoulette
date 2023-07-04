2beffece056f5b38c5c603c22e426293daa5e4d1
Lee-Roy <ltd1999@hotmail.co.uk>
2023-07-04 13:43:14 +0100

    Added Website

---
+++ strats.js
@@ -0,0 +1,63 @@
+var strats = [
+    {
+        id: 0,
+        text: "Play a team of all guardians.",
+    },
+    {
+        id: 1,
+        text: "Play a team of all warriors.",
+    },
+    {
+        id: 2,
+        text: "Play a team of all mages.",
+    },
+    {
+        id: 3,
+        text: "Play a team of all assasins.",
+    },
+    {
+        id: 4,
+        text: "Play a team of all hunters.",
+    },
+    {
+        id: 5,
+        text: "Waiting for the Train: You can only leave the base by using the teleport relic.",
+    },
+    {
+        id: 6,
+        text: "Bronze Time: Everyone uses /rgod and uses the build given.",
+    },
+    {
+        id: 7,
+        text: "Endgame: The player at the top of the list in the pre-game lobby is deemed Captain America. At any point in the game, Captain America can say ''Avengers Assemble!'' No matter what they're doing or where they are, all his teammates must rush to his side when he says this, and stay by him until he dismisses them. If Captain America dies while the Avengers are assembled, they cannot disband until they either avenge Captain America's death or die trying.",
+    },
+    {
+        id: 8,
+        text: "Gentlemen: You may never hit a female god with a basic attack, if you do, you must offer your life as a sacrifice to the poor lady you dared lay a finger on.",
+    },
+    {
+        id: 9,
+        text: "Opposite Day: Your team must consist only of tanky Assassins and damage Guardians.",
+    },
+    {
+        id: 10,
+        text: "Outcarry Kali: Have someone on your team pick Kali. Her teammates must try to prevent her from killing any of her targets.",
+    },
+    {
+        id: 11,
+        text: "You Are My Love: The ADC and Support are now a couple. They must stay near each other at all times. If one of them dies, the other must return to base and stay there until their lover respawns.",
+    },
+    {
+        id: 12,
+        text: "Dormant: Everyone gets their own random number, 1-4. Whatever number they get, they can't use that ability for the entire game.",
+    },
+    {
+        id: 13,
+        text: "Snakes in the Jungle: No one is allowed to enter the jungle unless the Jungler escorts them through it.",
+    },
+    {
+        id: 14,
+        text: "Profit Share: Everyone must vehemently attempt to steal kills.",
+    }
+
+];
